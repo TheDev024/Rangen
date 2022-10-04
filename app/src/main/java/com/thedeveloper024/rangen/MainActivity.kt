f2a5d7f5441283c393ef0bf7010d60a4c0761ef3
Abdullah Mahmudov <abdullahmahmudov024@gmail.com>
2022-10-04 23:18:39 +0400

    some layout properties changed and start functionality added

--- app/src/main/java/com/thedeveloper024/rangen/MainActivity.kt
+++ app/src/main/java/com/thedeveloper024/rangen/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.thedeveloper024.rangen
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.thedeveloper024.rangen.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fun displayResult(num: Int) {
+            val str_number = NumberFormat.getNumberInstance().format(num)
+            binding.generatedNumber.text = getString(R.string.generated_number, str_number)
+        }
+
+        binding.startButton.setOnClickListener {
+            displayResult(0)
+            binding.title.visibility = View.GONE
+            binding.startButton.visibility = View.GONE
+            binding.runLayout.visibility = View.VISIBLE
+        }
     }
 }
