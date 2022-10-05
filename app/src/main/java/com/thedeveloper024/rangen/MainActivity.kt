a4dfd10041206e5467585a57ebddfcdd891d2327
Abdullah Mahmudov <abdullahmahmudov024@gmail.com>
2022-10-05 10:05:30 +0400

    functionalities completed

--- app/src/main/java/com/thedeveloper024/rangen/MainActivity.kt
+++ app/src/main/java/com/thedeveloper024/rangen/MainActivity.kt
@@ -2,14 +2,18 @@ package com.thedeveloper024.rangen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
 import com.thedeveloper024.rangen.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-    lateinit var generator: RandomGenerator
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var generator: RandomGenerator
+    private var randomResult: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,28 +21,53 @@ class MainActivity : AppCompatActivity() {
         generator = RandomGenerator(0, 100)
         setContentView(binding.root)
 
-        fun displayResult(num: Int) {
-            val strNumber = NumberFormat.getNumberInstance().format(num)
-            binding.generatedNumber.text = getString(R.string.generated_number, strNumber)
-        }
-
         binding.startButton.setOnClickListener {
-            displayResult(0)
+            displayResult()
             binding.title.visibility = View.GONE
             binding.startButton.visibility = View.GONE
             binding.runLayout.visibility = View.VISIBLE
         }
 
         binding.minInputLayout.editText?.doOnTextChanged { _, _, _, _ ->
-            generator.min = binding.minInputField.text.toString().toInt()
+            if (binding.minInputField.text.toString() != "")
+                generator.min = binding.minInputField.text.toString().toInt()
+            else generator.min = 0
         }
 
         binding.maxInputLayout.editText?.doOnTextChanged { _, _, _, _ ->
-            generator.max = binding.maxInputField.text.toString().toInt()
+            if (binding.maxInputField.text.toString() != "")
+                generator.max = binding.maxInputField.text.toString().toInt()
+            else generator.max = 0
         }
 
         binding.generateButton.setOnClickListener {
-            displayResult(generator.generate())
+            generateNumber()
+        }
+    }
+
+    private fun displayResult() {
+        val strNumber = NumberFormat.getNumberInstance().format(randomResult)
+        when (strNumber.length) {
+            6 -> binding.generatedNumber.setTextSize(
+                TypedValue.COMPLEX_UNIT_SP, 148f
+            )
+            5 -> binding.generatedNumber.setTextSize(
+                TypedValue.COMPLEX_UNIT_SP, 186f
+            )
+            else -> binding.generatedNumber.setTextSize(
+                TypedValue.COMPLEX_UNIT_SP, 224f
+            )
+        }
+        binding.generatedNumber.text = getString(R.string.generated_number, strNumber)
+    }
+
+    private fun generateNumber() {
+        if (generator.min >= generator.max) {
+            val message = Toast.makeText(this, "Values are not correct!", Toast.LENGTH_SHORT)
+            message.show()
+        } else {
+            randomResult = generator.generate()
+            displayResult()
         }
     }
 }
